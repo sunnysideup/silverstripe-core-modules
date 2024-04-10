ca730b43d360fd93ebf4b0afe5c21fba8bfa4ac7
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-04-10 20:42:05 +1200

    ENH: check for MFA settings

--- _config.php
+++ _config.php
@@ -1,1 +1,9 @@
 <?php
+
+use SilverStripe\Core\Environment;
+
+if (isset($_SERVER['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/admin/')) {
+    if(! Environment::getEnv('SS_MFA_SECRET_KEY')) {
+        user_error('Make sure to complete MFA Settings');
+    }
+}
