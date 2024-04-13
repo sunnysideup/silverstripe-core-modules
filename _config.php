00af06afb48c19d5464074300ab55f6167cec507
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-04-13 16:09:31 +1200

    DEP: composer update

--- _config.php
+++ _config.php
@@ -1,9 +1,12 @@
 <?php
 
+use SilverStripe\Control\Director;
 use SilverStripe\Core\Environment;
 
 if (isset($_SERVER['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/admin/')) {
     if(! Environment::getEnv('SS_MFA_SECRET_KEY')) {
-        user_error('Make sure to complete MFA Settings');
+        if(! Director::isDev()) {
+            user_error('Make sure to complete MFA Settings');
+        }
     }
 }
