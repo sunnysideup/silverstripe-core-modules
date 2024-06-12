e53b4a092d28d9c57cdb181d46edff1d42db7bbd
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-06-13 08:49:38 +1200

    FIX: linting

--- _config.php
+++ _config.php
@@ -3,10 +3,6 @@
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Environment;
 
-if (isset($_SERVER['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/admin/')) {
-    if(! Environment::getEnv('SS_MFA_SECRET_KEY')) {
-        if(! Director::isDev()) {
-            user_error('Make sure to complete MFA Settings');
-        }
-    }
+if (isset($_SERVER['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/admin/') && ! Environment::getEnv('SS_MFA_SECRET_KEY') && ! Director::isDev()) {
+    user_error('Make sure to complete MFA Settings');
 }
