43b23f82233af5168a1870dd6f6ee4b47a30f4f0
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-12-09 13:01:25 +1300

    FIX: fix up task

--- src/Tasks/PruneProjectComposerRequirements.php
+++ src/Tasks/PruneProjectComposerRequirements.php
@@ -17,6 +17,16 @@ class PruneProjectComposerRequirements extends BuildTask
 
     private static $segment = 'prune-project-composer-requirements';
 
+    private static $packages_to_skip = [
+        'silverstripe/recipe-cms',
+        'silverstripe/recipe-core',
+        'silverstripe/vendor-plugin',
+        'silverstripe/mimevalidator',
+        'silverstripe/recipe-plugin',
+        'sunnysideup/core-modules',
+        'php',
+    ];
+
     public function run($request)
     {
         // Base folder containing composer.json and vendor directory
@@ -33,6 +43,7 @@ class PruneProjectComposerRequirements extends BuildTask
     protected function removeUnusedPackages(string $basePath): void
     {
         $baseComposerPath = rtrim($basePath, '/') . '/composer.json';
+        $skip = $this->config()->get('packages_to_skip');
 
         if (!file_exists($baseComposerPath)) {
             throw new RuntimeException('Base composer.json not found at: ' . $baseComposerPath);
@@ -50,53 +61,74 @@ class PruneProjectComposerRequirements extends BuildTask
         }
 
         $baseRequire = $baseComposer['require'];
-        $vendorDir = $basePath . '/vendor';
 
-        if (!is_dir($vendorDir)) {
-            throw new RuntimeException('Vendor directory not found: ' . $vendorDir);
+
+        $composerFiles = $this->getComposerFiles();
+
+        foreach ($composerFiles as $packageComposerPath) {
+            $packageComposer = json_decode(file_get_contents($packageComposerPath), true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                DB::alteration_message('Skipping invalid JSON file: ' . $packageComposerPath, 'error');
+                continue;
+            }
+
+            if (!isset($packageComposer['require'])) {
+                DB::alteration_message('Skipping file without "require" section: ' . $packageComposerPath);
+                continue;
+            }
+
+            $packageRequire = $packageComposer['require'];
+            $packageName = $packageComposer['name'] ?? 'unknown';
+            foreach (array_keys($packageRequire) as $package) {
+                if (in_array($package, $skip)) {
+                    continue;
+                }
+                if (isset($baseRequire[$package])) {
+                    DB::alteration_message('Removing ' . $package . ' from ' . $baseComposerPath . ' as it is also required by '.$packageName, 'deleted');
+                    unset($baseRequire[$package]);
+                }
+            }
         }
 
-        // Loop through every composer.json in vendor/vendor-name/package-name
+        $baseComposer['require'] = $baseRequire;
+
+        $jsonData = json_encode($baseComposer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        if ($jsonData === false) {
+            throw new RuntimeException('Failed to encode JSON for base composer.json: ' . json_last_error_msg());
+        }
+
+        file_put_contents($baseComposerPath, $jsonData);
+
+        DB::alteration_message('Unused packages removed from ' . $baseComposerPath, 'created');
+    }
+
+    protected function getComposerFiles(): array
+    {
+        $composerFiles = [];
+        $vendorDir = $this->getVendorDir();
         $iterator = new RecursiveIteratorIterator(
             new RecursiveDirectoryIterator($vendorDir, RecursiveDirectoryIterator::SKIP_DOTS)
         );
 
         foreach ($iterator as $file) {
-            if ($file->getFilename() === 'composer.json') {
-                $packageComposerPath = $file->getPathname();
-                $packageComposer = json_decode(file_get_contents($packageComposerPath), true);
+            if ($file->isFile() && $file->getFilename() === 'composer.json') {
+                $relativePath = str_replace($vendorDir . '/', '', $file->getPath());
+                $pathParts = explode('/', $relativePath);
 
-                if (json_last_error() !== JSON_ERROR_NONE) {
-                    DB::alteration_message('Skipping invalid JSON file: ' . $packageComposerPath);
-                    continue;
-                }
-
-                if (!isset($packageComposer['require'])) {
-                    DB::alteration_message('Skipping file without "require" section: ' . $packageComposerPath);
-                    continue;
-                }
-
-                $packageRequire = $packageComposer['require'];
-
-                // Remove packages from base require if they are not used in the package
-                foreach ($baseRequire as $package => $version) {
-                    if (!isset($packageRequire[$package])) {
-                        DB::alteration_message('Removing ' . $package . ' from ' . $packageComposerPath, 'deleted');
-                        unset($baseRequire[$package]);
-                    }
+                // Ensure the path matches vendor/vendor-name/package-name/composer.json
+                if (count($pathParts) === 2) {
+                    $composerFiles[] = $file->getRealPath();
                 }
             }
         }
-
-        // Update the base composer.json
-        $baseComposer['require'] = $baseRequire;
-        file_put_contents(
-            $baseComposerPath,
-            json_encode($baseComposer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
-        );
-
-        DB::alteration_message('Unused packages removed from ' . $baseComposerPath);
+        return $composerFiles;
     }
 
+    public function getVendorDir(): string
+    {
+        return Director::baseFolder() . '/vendor';
+    }
 
 }
