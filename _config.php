c9bbf058105161a61fd261dbc87effc4730755e0
Nicolaas <github@sunnysideup.co.nz>
2025-02-13 13:52:39 +1300

    FIX: enforce hybrid sessions

--- _config.php
+++ _config.php
@@ -3,6 +3,15 @@
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Environment;
 
-if (isset($_SERVER['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/admin/') && ! Environment::getEnv('SS_MFA_SECRET_KEY') && ! Director::isDev()) {
-    user_error('Make sure to complete MFA Settings');
+if (! Director::isDev()) {
+    if (isset($_GET['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/dev/') || Environment::isCli()) {
+        if (! Environment::getEnv('SS_MFA_SECRET_KEY')) {
+            user_error('Make sure to complete MFA Settings');
+        }
+        if (class_exists('SilverStripe\HybridSessions\HybridSession')) {
+            if (! Environment::getEnv('SS_SESSION_KEY')) {
+                user_error('Make sure to complete HybridSession Settings - see: https://github.com/silverstripe/silverstripe-hybridsessions');
+            }
+        }
+    }
 }
