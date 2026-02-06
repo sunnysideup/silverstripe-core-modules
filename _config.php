1e845b2461e37b52c543111aa4ec260a7de1486c
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-02-07 12:19:38 +1300

    FIX: lint

--- _config.php
+++ _config.php
@@ -15,22 +15,18 @@ if (Director::isDev()) {
         }
         unset($allowed, $ip);
     }
-} else {
-    if (isset($_GET['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/dev/') || Environment::isCli()) {
-        if (! Environment::getEnv('SS_MFA_SECRET_KEY')) {
-            user_error(
-                '
+} elseif (isset($_GET['REQUEST_URI']) && 0 === strpos($_SERVER['REQUEST_URI'], '/dev/') || Environment::isCli()) {
+    if (! Environment::getEnv('SS_MFA_SECRET_KEY')) {
+        user_error(
+            '
                 Make sure to complete MFA Settings - add a SS_MFA_SECRET_KEY to your .env file'
-            );
-        }
-        if (class_exists('SilverStripe\HybridSessions\HybridSession')) {
-            if (! Environment::getEnv('SS_SESSION_KEY')) {
-                user_error('
+        );
+    }
+    if (class_exists('SilverStripe\HybridSessions\HybridSession') && ! Environment::getEnv('SS_SESSION_KEY')) {
+        user_error('
                     Make sure to complete HybridSession
                     Add SS_SESSION_KEY to your .env file.
                     Also see:  https://github.com/silverstripe/silverstripe-hybridsessions
                 ');
-            }
-        }
     }
 }
